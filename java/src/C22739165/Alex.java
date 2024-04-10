ae3d0432960b416d6f7dbb8b4494164d5b926e1a
AlexMackey1 <C22739165@mytudublin.ie>
2024-04-10 15:54:11 +0100

    Testing adding images to the visual

--- java/src/C22739165/Alex.java
+++ java/src/C22739165/Alex.java
@@ -1,26 +1,49 @@
 package C22739165;
 
 import ie.tudublin.Visual;
+import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Alex {
-    Visual visual; // Assuming Visual is your PApplet subclass
+    Visual visual;
+    int numBoxes = 20;
+    float angle = 0;
+    PImage bgImage; // For the background image
 
     public Alex(Visual visual) {
         this.visual = visual;
+        bgImage = visual.loadImage("java/data/test.jpg"); // Load the background image
+        if (bgImage != null) {
+            // Resize the image to match the application window size
+            bgImage.resize(visual.width, visual.height);
+        } else {
+            System.out.println("Failed to load test.jpg. Check if the file exists in the data folder.");
+        }
     }
+    
 
     public void draw() {
-        visual.background(0);
+        visual.background(bgImage); // Use the image as the background
         visual.calculateAverageAmplitude();
-        visual.stroke(visual.map(visual.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-        visual.strokeWeight(5);
+        float amplitude = visual.getSmoothedAmplitude();
+        
+        visual.stroke(visual.map(amplitude, 0, 1, 0, 255), 255, 255);
+        visual.strokeWeight(2);
         visual.noFill();
         visual.lights();
-        visual.pushMatrix();
-        visual.translate(visual.width / 2, visual.height / 2, -200);
-        visual.rotateX(visual.frameCount * 0.01f);
-        visual.rotateZ(visual.frameCount * 0.01f);
-        visual.box(50 + (200 * visual.getSmoothedAmplitude()));
-        visual.popMatrix();
+
+        float spacing = 360.0f / numBoxes;
+        for (int i = 0; i < numBoxes; i++) {
+            visual.pushMatrix();
+            visual.translate(visual.width / 2, visual.height / 2);
+            float currentAngle = PApplet.radians(spacing * i + angle);
+            visual.rotateX(currentAngle);
+            visual.rotateY(currentAngle);
+            float boxSize = 50 + (amplitude * 200);
+            visual.box(boxSize);
+            visual.popMatrix();
+        }
+
+        angle += 0.5;
     }
 }
