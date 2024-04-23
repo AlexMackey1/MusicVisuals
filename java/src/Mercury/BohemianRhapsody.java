16bf79af89e69ee3e120266a9abe3b4fbc9afab9
AlexMackey1 <C22739165@mytudublin.ie>
2024-04-23 14:52:43 +0100

    Began working out the timings

--- java/src/Mercury/BohemianRhapsody.java
+++ java/src/Mercury/BohemianRhapsody.java
@@ -7,7 +7,7 @@ import C22483302.SergeiAwesomeVisual;
 import C22483302.SergeiAwesomeVisual2;
 
 public class BohemianRhapsody extends Visual {
-    int mode = 1;
+    int mode = 0;
     Alex alex;
     AaronVisual aaron;
     SergeiAwesomeVisual sergei;
@@ -21,7 +21,7 @@ public class BohemianRhapsody extends Visual {
     public void setup() {
         startMinim();
         // Make sure the path is correct. It assumes the file is in the data folder.
-        loadAudio("java/data/Section3.mp3");
+        loadAudio("java/data/QueenBohemianRhapsody.mp3");
         getAudioPlayer().play();  // Start playing the audio for debugging
         colorMode(HSB);
         alex = new Alex(this); // Pass 'this' as the PApplet context
@@ -44,6 +44,30 @@ public class BohemianRhapsody extends Visual {
     }
 
     public void draw() {
+
+        if(getAudioPlayer().position() >= 0 && getAudioPlayer().position() <= 10)
+        {
+            mode = 0;
+        }
+        if(getAudioPlayer().position() >=117000  && getAudioPlayer().position() <= 117100)
+        {
+            mode = 3;
+        }
+        if(getAudioPlayer().position() >= 184000 && getAudioPlayer().position() <= 184100)
+        {
+            mode = 4;
+        }
+
+        if(getAudioPlayer().position() >= 294000 && getAudioPlayer().position() <= 294100)
+        {
+            mode = 5;
+        }
+
+        if(getAudioPlayer().position() >= 359000 && getAudioPlayer().position() <= 359100)
+        {
+            mode = 5;
+        }
+
         switch (mode) {
             case 0:
                 alex.draw();
